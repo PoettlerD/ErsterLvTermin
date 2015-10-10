78124c3f30049da4d84b104dbcc0033c75f80599
PoettlerD <david.poettler@gmx.at>
2015-10-10 15:19:40 +0200

    fgsg

---
+++ src/Demo.java
@@ -0,0 +1,26 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean treeFront = true;
+		int numberLeaves = 0;
+		
+		int a = 4;
+		int b = 2;
+		char meinZeichen = 'X';
+		System.out.println("Ist ein Baum voraus? " + treeFront);
+		System.out.println();
+		System.out.println("Anzahl der Blätter: " + numberLeaves);
+		System.out.println();
+		System.out.println("blablabla");
+		System.out.println(meinZeichen);
+		
+		System.out.println(a+" + "+b+ " = "+(a+b));
+		System.out.println(a+" - "+b+ " = "+(a-b));
+		System.out.println(a+" * "+b+ " = "+(a*b));
+		System.out.println(a+" / "+b+ " = "+(a/b));
+	}
+
+}
